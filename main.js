721086a92e548ff12d2380e2d7b9dfd9a7a4c064
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-22 16:58:30 +0100

    tentativi di ricevere una stampa

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-'use strict;'
+'use strict';
+
+//variabili
+
+const prezzoKm = 0.21
+const etaUtente = document.getElementById(eta);
+const kmUtente = document.getElementById(distanza);
+
+console.log(etaUtente);
+console.log(kmUtente);
