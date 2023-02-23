afb98e90b74eca8becd94801f46699a8328ba48e
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-23 16:50:29 +0100

    correzione

--- main.js
+++ main.js
@@ -19,3 +19,35 @@
 
 //da qui il nuovo tentativo post-correzione
 
+//inizializzo le var degli elementi/oggetti html che mi servono
+const userName = document.getElementById('name');
+const userKm = document.getElementById('km');
+const userAge = document.getElementById('eta');
+const btnStart = document.getElementById('genera');
+
+//console.log(userName, userKm, userAge);
+
+//qui inizial
+
+btnStart.addEventListener ('click',
+function(){
+    const inputName = userName.value;
+    const inputKm = number(userKm.value);
+    const inputAge = userAge.value;
+    console.log(inputName, inputKm, inputAge);
+
+    let ticketPrice = inputKm * 0.21;
+
+    if(inputAge === 'under') {
+        ticketPrice = ticketPrice * 0.8;
+    }
+    else if(inputAge === 'over') {
+        ticketPrice = ticketPrice * 0.6;
+    }
+
+
+    ticketPrice = ticketPrice.toFixed(2);
+    console.log(ticketPrice);
+});
+
+
